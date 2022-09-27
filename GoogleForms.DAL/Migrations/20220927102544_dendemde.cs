f12832d7694209a7df450a4b3418d2661670e2dc
bunyamingurmuc <bunyamingurmuc@gmail.com>
2022-09-27 17:45:01 +0300

    dsadsadsadas

---
+++ GoogleForms.DAL/Migrations/20220927102544_dendemde.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoogleForms.DAL.Migrations
+{
+    public partial class dendemde : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6210));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6212));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6213));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6214));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6214));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6215));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6216));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6216));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6217));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6217));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6218));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6218));
+
+            migrationBuilder.UpdateData(
+                table: "Forms",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(5981));
+
+            migrationBuilder.UpdateData(
+                table: "Forms",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(5997));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6119));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6122));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6123));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 27, 13, 25, 43, 920, DateTimeKind.Local).AddTicks(6123));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3850));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3852));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3852));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3853));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3853));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3854));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3854));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3855));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3855));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3856));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3856));
+
+            migrationBuilder.UpdateData(
+                table: "Answers",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3857));
+
+            migrationBuilder.UpdateData(
+                table: "Forms",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3696));
+
+            migrationBuilder.UpdateData(
+                table: "Forms",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3709));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3803));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3806));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3807));
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2022, 9, 22, 19, 26, 48, 586, DateTimeKind.Local).AddTicks(3807));
+        }
+    }
+}
